d123cf857aa9ef0b9d846bddb8af55864687aa4f
Vincenzo Tramo <vv.tramo@gmail.com>
2024-12-27 17:06:17 +0100

    refactor: prevent multiple inclusions of ppl.hh by adding include guard

--- src/utils/ppl_aliases.h
+++ src/utils/ppl_aliases.h
@@ -5,7 +5,9 @@
 #ifndef PPL_ALIASES_H
 #define PPL_ALIASES_H
 
+#ifndef PPL_ppl_hh
 #include <ppl.hh>
+#endif
 
 using Poly = Parma_Polyhedra_Library::NNC_Polyhedron;
 using Powerset = Parma_Polyhedra_Library::Pointset_Powerset<Poly>;
