f88cd77c46aaa1fe8a2ad2f4dc29292202e93f83
Vincenzo Tramo <vv.tramo@gmail.com>
2024-12-28 22:10:18 +0100

    test(parser): add invalid input test cases

--- tests/parser/parse_system_tests.cpp
+++ tests/parser/parse_system_tests.cpp
@@ -1,13 +1,14 @@
-#include <ppl_utils.h>
 #include <catch2/catch_test_macros.hpp>
+#include "PolyhedralSystemParsingResult.h"
+#include "ppl_utils.h"
 #include "PolyhedralSystem.h"
 
-TEST_CASE("Correctly builds PolyhedralSystem from a string", "[parser]")
+TEST_CASE("Correctly parse PolyhedralSystem", "[good]")
 {
     SECTION("Building PolyhedralSystem with 'Flow' constraint first")
     {
-        const PolyhedralSystem polyhedralSystem {
-            buildPolyhedralSystem(
+        const PolyhedralSystemParsingResult polyhedralSystemParsingResult {
+            parsePolyhedralSystem(
                "Flow { X <= 4 }"
                "Inv ( { X + Y >= 3 & Y >= 4 } )"
                "p ( { X < 3 & Y <= 3 } { X < 3 & Y <= 10 } )"
@@ -15,14 +16,17 @@ TEST_CASE("Correctly builds PolyhedralSystem from a string", "[parser]")
            )
        };
 
+        REQUIRE(polyhedralSystemParsingResult.ok());
+
+        const PolyhedralSystem& polyhedralSystem { *polyhedralSystemParsingResult };
         REQUIRE(polyhedralSystem.getSpaceDimension() == 2);
         REQUIRE(polyhedralSystem.getTotalAtoms() == 2);
     }
 
     SECTION("Building PolyhedralSystem with 'Inv' constraint first")
     {
-        const PolyhedralSystem polyhedralSystem {
-            buildPolyhedralSystem(
+        const PolyhedralSystemParsingResult polyhedralSystemParsingResult {
+            parsePolyhedralSystem(
                "Inv ( { X + Y >= 3 & Y >= 4 } )"
                "Flow { X <= 4 }"
                "p ( { X < 3 & Y <= 3 } { X < 3 & Y <= 10 } )"
@@ -30,6 +34,9 @@ TEST_CASE("Correctly builds PolyhedralSystem from a string", "[parser]")
            )
        };
 
+        REQUIRE(polyhedralSystemParsingResult.ok());
+
+        const PolyhedralSystem& polyhedralSystem { *polyhedralSystemParsingResult };
         REQUIRE(polyhedralSystem.getSpaceDimension() == 2);
         REQUIRE(polyhedralSystem.getTotalAtoms() == 2);
     }
@@ -43,7 +50,7 @@ TEST_CASE("Correctly builds PolyhedralSystem from a string", "[parser]")
             "q { X > 3 & Y >= 4 }"
         };
         PolyhedralSystem polyhedralSystem {};
-        input >> polyhedralSystem;
+        input >> std::move(polyhedralSystem);
 
         REQUIRE(polyhedralSystem.getSpaceDimension() == 2);
         REQUIRE(polyhedralSystem.getTotalAtoms() == 2);
@@ -59,7 +66,7 @@ TEST_CASE("Correctly builds PolyhedralSystem from a string", "[parser]")
             "Flow { X <= 4 }"
         };
         PolyhedralSystem polyhedralSystem {};
-        input >> polyhedralSystem;
+        input >> std::move(polyhedralSystem);
 
         REQUIRE(polyhedralSystem.getSpaceDimension() == 2);
         REQUIRE(polyhedralSystem.getTotalAtoms() == 3);
@@ -76,7 +83,7 @@ TEST_CASE("Correctly builds PolyhedralSystem from a string", "[parser]")
             "Flow { X <= 4 }"
         };
         PolyhedralSystem polyhedralSystem {};
-        input >> polyhedralSystem;
+        input >> std::move(polyhedralSystem);
 
         REQUIRE(polyhedralSystem.getSpaceDimension() == 3);
         REQUIRE(polyhedralSystem.getTotalAtoms() == 4);
@@ -100,7 +107,7 @@ TEST_CASE("Correctly builds PolyhedralSystem from a string", "[parser]")
             "p3 { 4L < 1 & 4Z < 1 }"
             "u1 ( { 10X < 3 & 40Y <= 3 } { 99X > 10 & 21Y <= 2 } { 2M <= 10 } { 3X = 3 } )"
         };
-        input >> polyhedralSystem;
+        input >> std::move(polyhedralSystem);
 
         REQUIRE(polyhedralSystem.getSpaceDimension() == 6);
         REQUIRE(polyhedralSystem.getTotalAtoms() == 11);
@@ -137,10 +144,221 @@ TEST_CASE("Correctly builds PolyhedralSystem from a string", "[parser]")
 
 
         PolyhedralSystem polyhedralSystem {};
-        input >> polyhedralSystem;
+        input >> std::move(polyhedralSystem);
 
         REQUIRE(expectedPolyhedralSystem.getTotalAtoms() == polyhedralSystem.getTotalAtoms());
         REQUIRE(expectedPolyhedralSystem.getSpaceDimension() == polyhedralSystem.getSpaceDimension());
         REQUIRE(expectedPolyhedralSystem == polyhedralSystem);
+    }
+
+    SECTION("Read a PolyhedralSystem from a file, then parse, then compare with the expected PolyhedralSystem")
+    {
+        std::ifstream input;
+        std::string testCaseFilePath { "polyhedral-system-test-1.txt" };
+        input.open(testCaseFilePath);
+
+        PolyhedralSystem polyhedralSystem {};
+        input >> std::move(polyhedralSystem);
+    }
+}
+
+TEST_CASE("Correctly report syntax errors when parsing PolyhedralSystem", "[bad]")
+{
+    SECTION("Non-integer coefficient [TEST 1]")
+    {
+        const PolyhedralSystemParsingResult polyhedralSystemParsingResult {
+            parsePolyhedralSystem(
+               "Flow { X <= 4.0 }"
+               "Inv ( { X + Y >= 3 & Y >= 4 } )"
+               "p ( { X < 3 & Y <= 3 } { X < 3 & Y <= 10 } )"
+               "q { X > 3 & Y >= 4 }"
+           )
+        };
+
+        REQUIRE(!polyhedralSystemParsingResult.ok());
+        REQUIRE(polyhedralSystemParsingResult.errorCount() == 2);
+        REQUIRE(polyhedralSystemParsingResult.syntaxErrorCount() == 1);
+        REQUIRE(polyhedralSystemParsingResult.lexicalErrorCount() == 1);
+        REQUIRE(polyhedralSystemParsingResult.unknownErrorCount() == 0);
+        REQUIRE(polyhedralSystemParsingResult.lexicalErrors().size() == 1);
+        REQUIRE(polyhedralSystemParsingResult.syntaxErrors().size() == 1);
+        REQUIRE(polyhedralSystemParsingResult.unknownErrors().empty());
+
+        const PolyhedralSystemParserError nonIntegerError { polyhedralSystemParsingResult.lexicalErrors()[0] };
+        REQUIRE(nonIntegerError.line() == 1);
+        REQUIRE(nonIntegerError.charPositionInLine() == 13);
+    }
+
+    SECTION("Non-integer coefficient [TEST 2]")
+    {
+        const PolyhedralSystemParsingResult polyhedralSystemParsingResult {
+            parsePolyhedralSystem(
+               "Flow { X <= 4 }\n"
+               "Inv ( { X + Y >= 3 & Y >= 4 } )\n"
+               "p ( { X < 3.42 & Y <= 3 } { X < 3 & Y <= 10 } )\n"
+               "q { X > 3 & Y >= 4 }\n"
+           )
+        };
+
+        REQUIRE(!polyhedralSystemParsingResult.ok());
+        REQUIRE(polyhedralSystemParsingResult.errorCount() == 2);
+        REQUIRE(polyhedralSystemParsingResult.syntaxErrorCount() == 1);
+        REQUIRE(polyhedralSystemParsingResult.lexicalErrorCount() == 1);
+        REQUIRE(polyhedralSystemParsingResult.unknownErrorCount() == 0);
+        REQUIRE(polyhedralSystemParsingResult.lexicalErrors().size() == 1);
+        REQUIRE(polyhedralSystemParsingResult.syntaxErrors().size() == 1);
+        REQUIRE(polyhedralSystemParsingResult.unknownErrors().empty());
+
+        const PolyhedralSystemParserError nonIntegerError { polyhedralSystemParsingResult.lexicalErrors()[0] };
+        REQUIRE(nonIntegerError.line() == 3);
+        REQUIRE(nonIntegerError.charPositionInLine() == 11);
+    }
+
+    SECTION("Non-integer coefficient [TEST 3]")
+    {
+        const PolyhedralSystemParsingResult polyhedralSystemParsingResult {
+            parsePolyhedralSystem(
+               "Flow { X <= 4 }\n"
+               "p ( { X < 3 & Y <= 3 } { X < 3 & Y <= 10 } )\n"
+               "q { X > 3 & Y >= 4 }\n"
+               "Inv ( { X + Y >= 3.442312 & Y >= 42312.23213 } )\n"
+           )
+        };
+
+        REQUIRE(!polyhedralSystemParsingResult.ok());
+        REQUIRE(polyhedralSystemParsingResult.errorCount() == 3);
+        REQUIRE(polyhedralSystemParsingResult.lexicalErrorCount() == 2);
+        REQUIRE(polyhedralSystemParsingResult.syntaxErrorCount() == 1);
+        REQUIRE(polyhedralSystemParsingResult.lexicalErrors().size() == 2);
+        REQUIRE(polyhedralSystemParsingResult.syntaxErrors().size() == 1);
+        REQUIRE(polyhedralSystemParsingResult.unknownErrors().empty());
+
+        const PolyhedralSystemParserError firstNonIntegerError { polyhedralSystemParsingResult.lexicalErrors()[0] };
+        REQUIRE(firstNonIntegerError.line() == 4);
+        REQUIRE(firstNonIntegerError.charPositionInLine() == 18);
+    }
+
+    SECTION("Empty [TEST 1]")
+    {
+        const PolyhedralSystemParsingResult polyhedralSystemParsingResult { parsePolyhedralSystem("   ") };
+
+        REQUIRE(!polyhedralSystemParsingResult.ok());
+    }
+
+    SECTION("Empty [TEST 2]")
+    {
+        const PolyhedralSystemParsingResult polyhedralSystemParsingResult { parsePolyhedralSystem("") };
+
+        REQUIRE(!polyhedralSystemParsingResult.ok());
+    }
+
+    SECTION("No Sense input")
+    {
+        const PolyhedralSystemParsingResult polyhedralSystemParsingResult { parsePolyhedralSystem(
+            "asdfasdf FLow afa fa m42 Inv x<3\n"
+            "q { X > 3 & Y >= 4 }\n"
+            "best{]}"
+        ) };
+
+        REQUIRE(!polyhedralSystemParsingResult.ok());
+    }
+
+    SECTION("Invalid variable identifier")
+    {
+        const PolyhedralSystemParsingResult polyhedralSystemParsingResult {
+            parsePolyhedralSystem(
+                "Inv ( { X + Y + Z >= 3 & Y - Z >= 4 } )\n"
+                "Flow { X + Z <= 4 }\n"
+                "p ( { X + 2Z < 3 & 3Y <= 3 } { X < 3 & Y <= 10 } )\n"
+                "q ( { 10X < 3 & 40Y <= 3 } { 99X > 10 & 21Y <= 2 } { 2Y <= 10 } { 3X = 3 } )\n"
+                "r { 4X > 3 & 4Y >= 4 }\n"
+                "v { 4X < 1 & 4Z < 1 }\n"
+                "z ( { 10X < 3 & 40Y <= 3 } { 8X > 10 & 21Y <= 2 } { 2Y <= 10 } { 3X = 3 } )\n"
+                "x ( { 10X < 3 & 40Y <= 3 } { 99X > 10 & 21Y <= 2 } { 2Y <= 10 } { 3X = 3 } )\n"
+                "u ( { 10X < 3 & 40Y <= 3 } { 99X > 10 & 21Y <= 2 } { 2Y <= 10 } { 3X = 3 } )\n"
+                "p1 ( { 10X < 3 & 40YFAS <= 3 } { 99X > 10 & 21Y <= 2 } { 2Y <= 10 } { 3X = 3 } )\n" // <-- HERE (Line 10)
+                "p2 { 4K > 3 & 4Y >= 4 }\n"
+                "p3 { 4L < 1 & 4Z < 1 }\n"
+                "u1 ( { 10X < 3 & 40Y <= 3 } { 99X > 10 & 21Y <= 2 } { 2M <= 10 } { 3X = 3 } )\n"
+            )
+        };
+
+        REQUIRE(!polyhedralSystemParsingResult.ok());
+        REQUIRE(polyhedralSystemParsingResult.errorCount() == 2);
+        REQUIRE(polyhedralSystemParsingResult.lexicalErrorCount() == 0);
+        REQUIRE(polyhedralSystemParsingResult.syntaxErrorCount() == 2);
+        REQUIRE(polyhedralSystemParsingResult.lexicalErrors().empty());
+        REQUIRE(polyhedralSystemParsingResult.syntaxErrors().size() == 2);
+        REQUIRE(polyhedralSystemParsingResult.unknownErrors().empty());
+
+        const PolyhedralSystemParserError invalidVariableIdError { polyhedralSystemParsingResult.syntaxErrors()[0] };
+        REQUIRE(invalidVariableIdError.line() == 10);
+        REQUIRE(invalidVariableIdError.charPositionInLine() == 20);
+    }
+
+    SECTION("Parsing an invalid input with >>")
+    {
+        std::istringstream input {
+            "Flow { X <= 4 }"
+            "Inv ( { 1*X + Y >= 3 & Y >= 4 } )"
+            "pjfksx ( { X < 3 & 1Y <= 3 } { X < 3 & 1*Y <= 10 } )" // Invalid ID
+            "q { 1X > 3 & Y >= 4 }"
+        };
+
+        PolyhedralSystem polyhedralSystem {};
+        input >> std::move(polyhedralSystem);
+
+        REQUIRE(input.fail());
+        REQUIRE(polyhedralSystem.getFlow().is_universe());
+        REQUIRE(polyhedralSystem.getInvariant().is_universe());
+        REQUIRE(polyhedralSystem.getPreFlow().is_universe());
+        REQUIRE(polyhedralSystem.getSpaceDimension() == 0);
+        REQUIRE(polyhedralSystem.getTotalAtoms() == 0);
+    }
+
+    SECTION("Missing INV")
+    {
+        const PolyhedralSystemParsingResult polyhedralSystemParsingResult {
+            parsePolyhedralSystem(
+               "p ( { X < 3 & Y <= 3 } { X < 3 & Y <= 10 } )\n"
+               "Flow { X <= 4 }\n"
+               "q { X > 3 & Y >= 4 }\n"
+           )
+        };
+
+        REQUIRE(!polyhedralSystemParsingResult.ok());
+        REQUIRE(polyhedralSystemParsingResult.errorCount() == 1);
+        REQUIRE(polyhedralSystemParsingResult.lexicalErrorCount() == 0);
+        REQUIRE(polyhedralSystemParsingResult.syntaxErrorCount() == 1);
+        REQUIRE(polyhedralSystemParsingResult.lexicalErrors().empty());
+        REQUIRE(polyhedralSystemParsingResult.syntaxErrors().size() == 1);
+        REQUIRE(polyhedralSystemParsingResult.unknownErrors().empty());
+
+        const PolyhedralSystemParserError firstNonIntegerError { polyhedralSystemParsingResult.syntaxErrors()[0] };
+        REQUIRE(firstNonIntegerError.line() == 4);
+        REQUIRE(firstNonIntegerError.charPositionInLine() == 0);
+    }
+
+    SECTION("Missing FLOW")
+    {
+        const PolyhedralSystemParsingResult polyhedralSystemParsingResult {
+            parsePolyhedralSystem(
+               "p ( { X < 3 & Y <= 3 } { X < 3 & Y <= 10 } )\n"
+               "q { X > 3 & Y >= 4 }\n"
+               "Inv { X <= 4 }\n"
+           )
+        };
+
+        REQUIRE(!polyhedralSystemParsingResult.ok());
+        REQUIRE(polyhedralSystemParsingResult.errorCount() == 1);
+        REQUIRE(polyhedralSystemParsingResult.lexicalErrorCount() == 0);
+        REQUIRE(polyhedralSystemParsingResult.syntaxErrorCount() == 1);
+        REQUIRE(polyhedralSystemParsingResult.lexicalErrors().empty());
+        REQUIRE(polyhedralSystemParsingResult.syntaxErrors().size() == 1);
+        REQUIRE(polyhedralSystemParsingResult.unknownErrors().empty());
+
+        const PolyhedralSystemParserError firstNonIntegerError { polyhedralSystemParsingResult.syntaxErrors()[0] };
+        REQUIRE(firstNonIntegerError.line() == 3);
+        REQUIRE(firstNonIntegerError.charPositionInLine() == 4);
     }
 }
