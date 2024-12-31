f51acb3053fb1d59fa516f834b55795b567bbd55
Vincenzo Tramo <vv.tramo@gmail.com>
2024-12-31 03:08:00 +0100

    test(discretization): add Fp0 test case

--- tests/discretization/discretization_tests.cpp
+++ tests/discretization/discretization_tests.cpp
@@ -1,1 +1,26 @@
 #include <catch2/catch_test_macros.hpp>
+#include <spot/tl/parse.hh>
+
+#include "discretization.h"
+#include "spot_utils.h"
+
+TEST_CASE("Discretization tests", "[discretization]")
+{
+    SECTION("F p0")
+    {
+        spot::formula formula { spot::parse_infix_psl("F p0").f };
+        spot::formula expectedFormula {
+            imposeSingOpenLastProperty(
+                U(top(), And({
+                                ap("p0"),
+                                Or({ top(), sing() }),
+                                alive()
+                         }))
+            )
+        };
+
+        spot::formula discretizedFormula { discretize(std::move(formula)) };
+
+        REQUIRE(discretizedFormula == expectedFormula);
+    }
+}
