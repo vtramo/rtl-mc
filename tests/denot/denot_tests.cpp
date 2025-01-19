2f05eb1da2383e839d769ddd14360f2056269a29
Vincenzo Tramo <vv.tramo@gmail.com>
2025-01-19 15:57:04 +0100

    test: add NO GAP Denot test cases

--- tests/denot/denot_tests.cpp
+++ tests/denot/denot_tests.cpp
@@ -246,3 +246,242 @@ TEST_CASE("GAP Test Cases")
         REQUIRE(result == expectedResult);
     }
 }
+
+TEST_CASE("NO GAP Test Cases")
+{
+    PolyhedralSystemConstSharedPtr polyhedralSystem {
+        std::make_shared<PolyhedralSystem>(
+            std::move(
+                *parsePolyhedralSystem(
+                    "Inv ( { a >= 0 & b >= 0 } )"
+                    "Flow { a + b >= -2 & a + b <= 2 & a >= -1 & a <= 1 & b >= -2 & b <= 2 & t = 1 }"
+                    "p { a > b }"
+                    "q { b > a }"
+                    "t0 { t = 0 }"
+                    "t1 { t <= 10 }"
+                )
+            )
+        )
+    };
+    const PolyhedralSystemSymbolTable& symbolTable { polyhedralSystem->getSymbolTable() };
+    auto a { *symbolTable.getVariable("a") };
+    auto b { *symbolTable.getVariable("b") };
+    auto t { *symbolTable.getVariable("t") };
+
+
+    SECTION("NO GAP k=1")
+    {
+        DiscreteLtlFormula discreteLtlFormula { DiscreteFiniteLtlFormula::discretize(spot::parse_infix_psl("t0 & G(t1) & F(p & F(q))").f).toLtl() };
+        PolyhedralSystemFormulaDenotationMap polyhedralSystemFormulaDenotationMap { polyhedralSystem };
+        BackwardNFA backwardNfa { discreteLtlFormula, std::move(polyhedralSystemFormulaDenotationMap) };
+        Denot denot { polyhedralSystem, backwardNfa };
+
+        Powerset expectedResult {
+            PPLUtils::powerset({
+                { t == 0, b >= 0, -a + b > -30, a - b > 0 },
+                { t == 0, b >= 0, -a + b >= 0, a >= 0, a - b > -30 }
+            })
+        };
+
+        Powerset result { denot.run() };
+        REQUIRE(result == expectedResult);
+    }
+
+    SECTION("NO GAP k=2")
+    {
+        DiscreteLtlFormula discreteLtlFormula { DiscreteFiniteLtlFormula::discretize(
+            spot::parse_infix_psl(
+                "t0 & G(t1) & F(p & F(q & F(p)))"
+            ).f).toLtl()
+        };
+        PolyhedralSystemFormulaDenotationMap polyhedralSystemFormulaDenotationMap { polyhedralSystem };
+        BackwardNFA backwardNfa { discreteLtlFormula, std::move(polyhedralSystemFormulaDenotationMap) };
+        Denot denot { polyhedralSystem, backwardNfa };
+
+        Powerset expectedResult {
+            PPLUtils::powerset({
+                { t == 0, b >= 0, -a + b > -30, a - b > 0 },
+                { t == 0, b >= 0, -a + b >= 0, a >= 0, a - b > -30 }
+            })
+        };
+
+        Powerset result { denot.run() };
+        REQUIRE(result == expectedResult);
+    }
+
+    SECTION("NO GAP k=3")
+    {
+        DiscreteLtlFormula discreteLtlFormula { DiscreteFiniteLtlFormula::discretize(
+            spot::parse_infix_psl(
+                "t0 & G(t1) & F(p & F(q & F(p & F(q))))"
+            ).f).toLtl()
+        };
+        PolyhedralSystemFormulaDenotationMap polyhedralSystemFormulaDenotationMap { polyhedralSystem };
+        BackwardNFA backwardNfa { discreteLtlFormula, std::move(polyhedralSystemFormulaDenotationMap) };
+        Denot denot { polyhedralSystem, backwardNfa };
+
+        Powerset expectedResult {
+            PPLUtils::powerset({
+                { t == 0, b >= 0, -a + b > -30, a - b > 0 },
+                { t == 0, b >= 0, -a + b >= 0, a >= 0, a - b > -30 }
+            })
+        };
+
+        Powerset result { denot.run() };
+        REQUIRE(result == expectedResult);
+    }
+
+    SECTION("NO GAP k=4")
+    {
+        DiscreteLtlFormula discreteLtlFormula { DiscreteFiniteLtlFormula::discretize(
+            spot::parse_infix_psl(
+                "t0 & G(t1) & F(p & F(q & F(p & F(q & F(p)))))"
+            ).f).toLtl()
+        };
+        PolyhedralSystemFormulaDenotationMap polyhedralSystemFormulaDenotationMap { polyhedralSystem };
+        BackwardNFA backwardNfa { discreteLtlFormula, std::move(polyhedralSystemFormulaDenotationMap) };
+        Denot denot { polyhedralSystem, backwardNfa };
+
+        Powerset expectedResult {
+            PPLUtils::powerset({
+                { t == 0, b >= 0, -a + b > -30, a - b > 0 },
+                { t == 0, b >= 0, -a + b >= 0, a >= 0, a - b > -30 }
+            })
+        };
+
+        Powerset result { denot.run() };
+        REQUIRE(result == expectedResult);
+    }
+
+    SECTION("NO GAP k=5")
+    {
+        DiscreteLtlFormula discreteLtlFormula { DiscreteFiniteLtlFormula::discretize(
+            spot::parse_infix_psl(
+                "t0 & G(t1) & F(p & F(q & F(p & F(q & F(p & F(q))))))"
+            ).f).toLtl()
+        };
+        PolyhedralSystemFormulaDenotationMap polyhedralSystemFormulaDenotationMap { polyhedralSystem };
+        BackwardNFA backwardNfa { discreteLtlFormula, std::move(polyhedralSystemFormulaDenotationMap) };
+        Denot denot { polyhedralSystem, backwardNfa };
+
+        Powerset expectedResult {
+            PPLUtils::powerset({
+                { t == 0, b >= 0, -a + b > -30, a - b > 0 },
+                { t == 0, b >= 0, -a + b >= 0, a >= 0, a - b > -30 }
+            })
+        };
+
+        Powerset result { denot.run() };
+        REQUIRE(result == expectedResult);
+    }
+
+    SECTION("NO GAP k=6")
+    {
+        DiscreteLtlFormula discreteLtlFormula { DiscreteFiniteLtlFormula::discretize(
+            spot::parse_infix_psl(
+                "t0 & G(t1) & F(p & F(q & F(p & F(q & F(p & F(q & F(p)))))))"
+            ).f).toLtl()
+        };
+        PolyhedralSystemFormulaDenotationMap polyhedralSystemFormulaDenotationMap { polyhedralSystem };
+        BackwardNFA backwardNfa { discreteLtlFormula, std::move(polyhedralSystemFormulaDenotationMap) };
+        Denot denot { polyhedralSystem, backwardNfa };
+
+        Powerset expectedResult {
+            PPLUtils::powerset({
+                { t == 0, b >= 0, -a + b > -30, a - b > 0 },
+                { t == 0, b >= 0, -a + b >= 0, a >= 0, a - b > -30 }
+            })
+        };
+
+        Powerset result { denot.run() };
+        REQUIRE(result == expectedResult);
+    }
+
+    SECTION("NO GAP k=7")
+    {
+        DiscreteLtlFormula discreteLtlFormula { DiscreteFiniteLtlFormula::discretize(
+            spot::parse_infix_psl(
+                "t0 & G(t1) & F(p & F(q & F(p & F(q & F(p & F(q & F(p & F(q))))))))"
+            ).f).toLtl()
+        };
+        PolyhedralSystemFormulaDenotationMap polyhedralSystemFormulaDenotationMap { polyhedralSystem };
+        BackwardNFA backwardNfa { discreteLtlFormula, std::move(polyhedralSystemFormulaDenotationMap) };
+        Denot denot { polyhedralSystem, backwardNfa };
+
+        Powerset expectedResult {
+            PPLUtils::powerset({
+                { t == 0, b >= 0, -a + b > -30, a - b > 0 },
+                { t == 0, b >= 0, -a + b >= 0, a >= 0, a - b > -30 }
+            })
+        };
+
+        Powerset result { denot.run() };
+        REQUIRE(result == expectedResult);
+    }
+
+    SECTION("NO GAP k=8")
+    {
+        DiscreteLtlFormula discreteLtlFormula { DiscreteFiniteLtlFormula::discretize(
+            spot::parse_infix_psl(
+                "t0 & G(t1) & F(p & F(q & F(p & F(q & F(p & F(q & F(p & F(q & F(p)))))))))"
+            ).f).toLtl()
+        };
+        PolyhedralSystemFormulaDenotationMap polyhedralSystemFormulaDenotationMap { polyhedralSystem };
+        BackwardNFA backwardNfa { discreteLtlFormula, std::move(polyhedralSystemFormulaDenotationMap) };
+        Denot denot { polyhedralSystem, backwardNfa };
+
+        Powerset expectedResult {
+            PPLUtils::powerset({
+                { t == 0, b >= 0, -a + b > -30, a - b > 0 },
+                { t == 0, b >= 0, -a + b >= 0, a >= 0, a - b > -30 }
+            })
+        };
+
+        Powerset result { denot.run() };
+        REQUIRE(result == expectedResult);
+    }
+
+    SECTION("NO GAP k=9")
+    {
+        DiscreteLtlFormula discreteLtlFormula { DiscreteFiniteLtlFormula::discretize(
+            spot::parse_infix_psl(
+                "t0 & G(t1) & F(p & F(q & F(p & F(q & F(p & F(q & F(p & F(q & F(p & F(q))))))))))"
+            ).f).toLtl()
+        };
+        PolyhedralSystemFormulaDenotationMap polyhedralSystemFormulaDenotationMap { polyhedralSystem };
+        BackwardNFA backwardNfa { discreteLtlFormula, std::move(polyhedralSystemFormulaDenotationMap) };
+        Denot denot { polyhedralSystem, backwardNfa };
+
+        Powerset expectedResult {
+            PPLUtils::powerset({
+                { t == 0, b >= 0, -a + b > -30, a - b > 0 },
+                { t == 0, b >= 0, -a + b >= 0, a >= 0, a - b > -30 }
+            })
+        };
+
+        Powerset result { denot.run() };
+        REQUIRE(result == expectedResult);
+    }
+
+    SECTION("NO GAP k=10")
+    {
+        DiscreteLtlFormula discreteLtlFormula { DiscreteFiniteLtlFormula::discretize(
+            spot::parse_infix_psl(
+                "t0 & G(t1) & F(p & F(q & F(p & F(q & F(p & F(q & F(p & F(q & F(p & F(q & F(p)))))))))))"
+            ).f).toLtl()
+        };
+        PolyhedralSystemFormulaDenotationMap polyhedralSystemFormulaDenotationMap { polyhedralSystem };
+        BackwardNFA backwardNfa { discreteLtlFormula, std::move(polyhedralSystemFormulaDenotationMap) };
+        Denot denot { polyhedralSystem, backwardNfa };
+
+        Powerset expectedResult {
+            PPLUtils::powerset({
+                { t == 0, b >= 0, -a + b > -30, a - b > 0 },
+                { t == 0, b >= 0, -a + b >= 0, a >= 0, a - b > -30 }
+            })
+        };
+
+        Powerset result { denot.run() };
+        REQUIRE(result == expectedResult);
+    }
+}
