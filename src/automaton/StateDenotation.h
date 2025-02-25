0cb40b1fa6e2adae0c3c618a231e25659cca8d9c
Vincenzo Tramo <vv.tramo@gmail.com>
2025-02-25 19:57:36 +0100

    feat(automaton): add labels method in StateDenotation
    StateDenotation now also stores state labels

--- src/automaton/StateDenotation.h
+++ src/automaton/StateDenotation.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <spdlog/spdlog.h>
+#include <spot/twa/formula2bdd.hh>
 
 #include "spot_utils.h"
 #include "ppl_output.h"
@@ -11,6 +12,7 @@ class StateDenotation
 public:
     [[nodiscard]] PowersetConstSharedPtr denotation() const { return m_denotation; }
     [[nodiscard]] const spot::formula& formula() const { return m_formula; }
+    [[nodiscard]] const spot::atomic_prop_set& labels() const { return *m_labels; }
     [[nodiscard]] bool isSingular() const { return m_isSing; }
     [[nodiscard]] bool isUniverse() const { return m_denotation->is_top(); }
     [[nodiscard]] bool isEmpty() const { return m_denotation->is_empty(); }
@@ -39,10 +41,19 @@ public:
         , m_denotation { std::move(denotation) }
         , m_isSing { isSing }
     {
+        m_labels = std::unique_ptr<spot::atomic_prop_set>(spot::atomic_prop_collect(m_formula));
+
+        if (!isSing)
+        {
+            removeSingFromLabels();
+        }
+
+        assert(!isSing || SpotUtils::containsSing(*m_labels));
     }
 
     StateDenotation(const StateDenotation& other)
         : m_formula { other.m_formula }
+        , m_labels { std::make_unique<spot::atomic_prop_set>(*other.m_labels) }
         , m_denotation { std::make_shared<Powerset>(*other.m_denotation) }
         , m_isSing { other.m_isSing }
     {
@@ -50,6 +61,7 @@ public:
 
     StateDenotation(StateDenotation&& other) noexcept
         : m_formula { std::move(other.m_formula) }
+        , m_labels { std::move(other.m_labels) }
         , m_denotation { std::move(other.m_denotation) }
         , m_isSing { other.isSingular() }
     {
@@ -57,6 +69,20 @@ public:
 
 private:
     spot::formula m_formula {};
+    std::unique_ptr<spot::atomic_prop_set> m_labels {};
+    bdd m_labelsAsBdd {};
     PowersetConstSharedPtr m_denotation {};
     bool m_isSing {};
+
+    void removeSingFromLabels() const
+    {
+        for (auto label { m_labels->begin() }; label != m_labels->end(); ++label)
+        {
+            if (*label == SpotUtils::sing())
+            {
+                m_labels->erase(label);
+                break;
+            }
+        }
+    }
 };
