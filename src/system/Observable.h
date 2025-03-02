f89ce6827990734ab885ea4a2688e7381994027b
Vincenzo Tramo <vv.tramo@gmail.com>
2025-03-02 19:42:41 +0100

    feat(system): add Observable class
    Introduces the `Observable` class, which models an observable in the polyhedral system. This class holds information about a set of atomic propositions and their interpretation, providing methods to access their properties, check conditions like whether the observable is empty or a universe, and to compute its hash. The class also supports custom string representations for its interpretation (for debugging).

---
+++ src/system/Observable.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <ppl_utils.h>
+#include <spot/tl/formula.hh>
+#include <spot/tl/apcollect.hh>
+#include <optional>
+
+#include "ppl_aliases.h"
+
+using PPL::IO_Operators::operator<<;
+
+class Observable {
+public:
+    Observable(spot::atomic_prop_set atoms, PowersetSharedPtr interpretation)
+        : m_atoms { std::make_shared<spot::atomic_prop_set>(std::move(atoms)) }
+        , m_interpretation { std::move(interpretation) }
+    {
+        m_hashcode = std::hash<std::shared_ptr<spot::atomic_prop_set>>{} (m_atoms);
+    }
+
+    Observable(spot::atomic_prop_set atoms, const Powerset& interpretation)
+        : m_atoms { std::make_shared<spot::atomic_prop_set>(std::move(atoms)) }
+        , m_interpretation { std::make_shared<Powerset>(interpretation) }
+    {
+        m_hashcode = std::hash<std::shared_ptr<spot::atomic_prop_set>>{} (m_atoms);
+    }
+
+    Observable(spot::atomic_prop_set atoms, PowersetSharedPtr interpretation, std::string interpretationToString)
+        : m_atoms { std::make_shared<spot::atomic_prop_set>(std::move(atoms)) }
+        , m_interpretation { std::move(interpretation) }
+        , m_interpretationToString { std::move(interpretationToString) }
+    {
+        m_hashcode = std::hash<std::shared_ptr<spot::atomic_prop_set>>{} (m_atoms);
+    }
+
+    Observable(spot::atomic_prop_set atoms, const Powerset& interpretation, std::string interpretationToString)
+        : m_atoms { std::make_shared<spot::atomic_prop_set>(std::move(atoms)) }
+        , m_interpretation { std::make_shared<Powerset>(interpretation) }
+        , m_interpretationToString { std::move(interpretationToString) }
+    {
+        m_hashcode = std::hash<std::shared_ptr<spot::atomic_prop_set>>{} (m_atoms);
+    }
+
+    Observable() = default;
+
+    [[nodiscard]] const spot::atomic_prop_set& atoms() const { return *m_atoms; }
+    [[nodiscard]] PowersetConstSharedPtr interpretation() const { return m_interpretation; }
+    [[nodiscard]] bool isUniverse() const { return m_interpretation->is_universe(); }
+    [[nodiscard]] bool isEmpty() const { return m_interpretation->is_empty(); }
+    [[nodiscard]] int totalAtoms() const { return m_atoms->size(); }
+    [[nodiscard]] int totalPatches() const { return m_interpretation->size(); }
+    [[nodiscard]] std::size_t hashcode() const { return m_hashcode; }
+
+    friend std::ostream& operator<< (std::ostream& os, const Observable& observable)
+    {
+        bool first = true;
+        os << "Observable: {";
+        for (const auto& atom: observable.atoms())
+        {
+            os << (first ? "" : ", ") << atom;
+            first = false;
+        }
+        os << "}\n";
+        os << "Interpretation: " <<
+            (observable.m_interpretationToString.has_value()
+                ? *observable.m_interpretationToString
+                : PPLUtils::toString(*observable.interpretation()));
+        return os;
+    }
+
+private:
+    std::shared_ptr<spot::atomic_prop_set> m_atoms {};
+    PowersetConstSharedPtr m_interpretation {};
+    std::optional<std::string> m_interpretationToString {};
+    std::size_t m_hashcode {};
+};
+
+inline bool operator== (const Observable& observable1, const Observable& observable2) {
+    return observable1.atoms() == observable2.atoms() &&
+           *observable1.interpretation() == *observable2.interpretation();
+}
+
+inline bool operator!= (const Observable& observable1, const Observable& observable2) {
+    return !(observable1 == observable2);
+}
+
+template<> struct std::hash<Observable>
+{
+    std::size_t operator() (const Observable& observable) const noexcept
+    {
+        return observable.hashcode();
+    }
+};
