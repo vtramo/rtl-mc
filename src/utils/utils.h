110106adb9579e4cf1c3819b0eabc324653152b4
Vincenzo Tramo <vv.tramo@gmail.com>
2025-03-03 13:04:38 +0100

    feat(utils): add hashCombine utility function

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -45,3 +45,10 @@ std::vector<std::set<T>> combinations(Iterator begin, Iterator end, const int n,
 
     return combinations;
 }
+
+template <typename T, typename... Rest>
+void hashCombine(std::size_t& seed, const T& v, const Rest&... rest)
+{
+    seed ^= std::hash<T>{}(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    (hashCombine(seed, rest), ...);
+}
