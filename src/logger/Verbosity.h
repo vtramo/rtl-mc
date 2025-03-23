5c1363a009973115f3488f0322c4b36588a229c1
Vincenzo Tramo <vv.tramo@gmail.com>
2025-03-23 18:56:34 +0100

    docs(logger): add documentation for package 'logger'

--- src/logger/Verbosity.h
+++ src/logger/Verbosity.h
@@ -1,11 +1,17 @@
 #pragma once
 
+/*!
+ *  \brief Enumeration representing different levels of logging verbosity.
+ *
+ * This enum defines the verbosity levels for logging, ranging from `silent` (no logging) to `trace` (most detailed logging).
+ * The levels are ordered, with higher values indicating more detailed logging.
+ */
 enum class Verbosity: int {
-    silent = 0,
-    verbose = 1,
-    veryVerbose = 2,
-    debug = 3,
-    trace = 4,
+    silent = 0,       //!< No logging output.
+    verbose = 1,      //!< Basic logging output.
+    veryVerbose = 2,  //!< More detailed logging output.
+    debug = 3,        //!< Debug-level logging output.
+    trace = 4,        //!< Most detailed logging output (e.g., for tracing execution).
 
-    totVerbosityLevels
+    totVerbosityLevels //!< Sentinel value representing the total number of verbosity levels.
 };
--- src/logger/logger.h
+++ src/logger/logger.h
@@ -8,27 +8,44 @@
 #include "DiscreteLtlFormula.h"
 #include "BackwardNFA.h"
 
-
+// Specializations of fmt::formatter to enable {fmt} library formatting for custom types using operator<<.
 template <> struct fmt::formatter<spot::formula> : fmt::ostream_formatter {};
 template <> struct fmt::formatter<PolyhedralSystem> : fmt::ostream_formatter {};
 template <> struct fmt::formatter<DiscreteLtlFormula> : fmt::ostream_formatter {};
 template <> struct fmt::formatter<BackwardNFA> : fmt::ostream_formatter {};
 
+/*!
+ *  \brief Namespace for logging utilities, providing verbosity control and logging functions.
+ */
 namespace Log
 {
-    inline Verbosity g_loggerVerbosity { Verbosity::silent };
+    inline Verbosity g_loggerVerbosity { Verbosity::silent }; //!< Global verbosity level for logging.
 
+    /*!
+     *  \brief Sets the global verbosity level for logging.
+     *  \param loggerVerbosity The desired verbosity level.
+     */
     inline void setVerbosityLevel(const Verbosity loggerVerbosity)
     {
         g_loggerVerbosity = loggerVerbosity;
     }
 
+    /*!
+     *  \brief Configures the logger with a simple pattern and default log level.
+     */
     inline void configureLogger()
     {
         spdlog::set_pattern("%v");
         spdlog::set_level(spdlog::level::info);
     }
 
+    /*!
+     *  \brief Logs a message if the global verbosity level is sufficient.
+     *  \tparam Args Types of the format arguments.
+     *  \param verbosity The verbosity level of the message.
+     *  \param message The message to log, optionally with format placeholders.
+     *  \param args Arguments to format into the message.
+     */
     template <typename ... Args>
     void log(const Verbosity verbosity, std::string_view message, Args&& ... args)
     {
