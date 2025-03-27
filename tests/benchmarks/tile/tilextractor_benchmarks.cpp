bfca2f1db91b0490aae1f477fa798d5d2bd9cd7d
Vincenzo Tramo <vv.tramo@gmail.com>
2025-03-27 14:37:27 +0100

    test(tile): add a new tilextractor test case and benchmark

--- tests/benchmarks/tile/tilextractor_benchmarks.cpp
+++ tests/benchmarks/tile/tilextractor_benchmarks.cpp
@@ -2,7 +2,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/benchmark/catch_benchmark.hpp>
 #include "test_utils.h"
-#include "spot_utils.h"
 #include "formula.h"
 #include "TileExtractor.h"
 #include "TileExtractorComplex.h"
@@ -11,7 +10,7 @@
 
 using namespace SpotUtils;
 
-TEST_CASE("TileExtractor benchmarks")
+TEST_CASE("TileExtractor benchmarks 1")
 {
     PPL::Variable x { 0 };
     PPL::Variable y { 1 };
@@ -156,5 +155,156 @@ TEST_CASE("TileExtractor benchmarks")
         };
 
         REQUIRE(tiles.size() == 5);
+    }
+}
+
+
+
+TEST_CASE("TileExtractor benchmarks 2")
+{
+    PPL::Variable x { 0 };
+    PPL::Variable y { 1 };
+
+    Powerset observableInterpretation {
+        PPLUtils::powerset({
+            { x >= -1, x <= 1, y >= -1, y <= 1 },
+            { x >= 1 , x <= 2, y >= 0 , y <= 1 },
+
+            { x >= 4 , x <= 5, y >= 0 , y <= 1 },
+            { x >= 4 , x <= 6, y >= 1 , y <= 2 },
+            { x >= 4 , x <= 7, y >= 2 , y <= 3 },
+            { x >= 4 , x <= 8, y >= 3 , y <= 4 },
+            { x >= 4 , x <= 9, y >= 4 , y <= 5 },
+            { x >= 4 , x <= 10, y >= 5 , y <= 6 },
+            { x >= 4 , x <= 11, y >= 7 , y <= 8 },
+            { x >= 4 , x <= 12, y >= 8 , y <= 9 },
+            { x >= 4 , x <= 13, y >= 9 , y <= 10 },
+
+            { x >= 10 , x <= 11, y >= 0 , y <= 1 },
+            { x >= 10 , x <= 12, y >= 1 , y <= 2 },
+            { x >= 10 , x <= 13, y >= 2 , y <= 3 },
+            { x >= 10 , x <= 14, y >= 3 , y <= 4 },
+            { x >= 10 , x <= 15, y >= 4 , y <= 5 },
+            { x >= 10 , x <= 16, y >= 5 , y <= 6 },
+
+            { x >= 0 , x <= 1, y >= 7 , y <= 8 },
+            { x >= 0 , x <= 1, y >= 8 , y <= 9 },
+            { x >= 0 , x <= 2, y >= 8 , y <= 9 },
+            { x >= 0 , x <= 3, y >= 9 , y <= 10 },
+            { x >= 2 , x <= 3, y >= 9 , y <= 10 },
+            { x >= 3 , x <= 4, y >= 9 , y <= 10 },
+
+            { x >= -1 , x <= 0, y >= 6 , y <= 7 },
+            { x >= -2 , x <= -1, y >= 5 , y <= 6 },
+            { x >= -2 , x <= -1, y >= 6 , y <= 7 },
+            { x >= 15 , x <= 16, y >= 3 , y <= 4 },
+            { x >= 15 , x <= 17, y >= 4 , y <= 5 },
+
+            { x >= 17 , x <= 18, y >= 5 , y <= 6 },
+            { x >= 17 , x <= 18, y >= 4 , y <= 5 },
+            { x >= 17 , x <= 18, y >= 3 , y <= 4 },
+            { x >= -3 , x <= -2, y >= 3 , y <= 4 },
+            { x >= -3 , x <= -2, y >= 4 , y <= 5 },
+
+            { x >= 50 , x <= 52, y >= 50 , y <= 50 },
+            { x >= 50 , x <= 53, y >= 50 , y <= 52 },
+            { x >= 50 , x <= 54, y >= 52 , y <= 53 },
+            { x >= 50 , x <= 55, y >= 53 , y <= 54 },
+            { x >= 50 , x <= 56, y >= 54 , y <= 55 },
+            { x >= 50 , x <= 57, y >= 55 , y <= 56 },
+            { x >= 50 , x <= 58, y >= 56 , y <= 57 },
+            { x >= 50 , x <= 59, y >= 57 , y <= 58 },
+
+            { x >= 70 , x <= 72, y >= 70 , y <= 70 },
+            { x >= 70 , x <= 73, y >= 70 , y <= 72 },
+            { x >= 70 , x <= 74, y >= 72 , y <= 73 },
+            { x >= 70 , x <= 75, y >= 73 , y <= 74 },
+            { x >= 70 , x <= 76, y >= 74 , y <= 75 },
+            { x >= 70 , x <= 77, y >= 75 , y <= 76 },
+            { x >= 70 , x <= 78, y >= 76 , y <= 77 },
+            { x >= 70 , x <= 79, y >= 77 , y <= 78 },
+
+            { x >= 90 , x <= 92, y >= 90 , y <= 90 },
+            { x >= 90 , x <= 93, y >= 90 , y <= 92 },
+            { x >= 90 , x <= 94, y >= 92 , y <= 93 },
+            { x >= 90 , x <= 95, y >= 93 , y <= 94 },
+            { x >= 90 , x <= 96, y >= 94 , y <= 95 },
+            { x >= 90 , x <= 97, y >= 95 , y <= 96 },
+            { x >= 90 , x <= 98, y >= 96 , y <= 97 },
+            { x >= 90 , x <= 99, y >= 97 , y <= 98 },
+
+            { x >= 110 , x <= 112, y >= 110 , y <= 110 },
+            { x >= 110 , x <= 113, y >= 110 , y <= 112 },
+            { x >= 110 , x <= 114, y >= 112 , y <= 113 },
+            { x >= 110 , x <= 115, y >= 113 , y <= 114 },
+            { x >= 110 , x <= 116, y >= 114 , y <= 115 },
+            { x >= 110 , x <= 117, y >= 115 , y <= 116 },
+            { x >= 110 , x <= 118, y >= 116 , y <= 117 },
+            { x >= 110 , x <= 119, y >= 117 , y <= 118 },
+
+            { x >= 110 , x <= 120, y >= 1 , y <= 2 },
+            { x >= 110 , x <= 121, y >= 2 , y <= 3 },
+            { x >= 110 , x <= 122, y >= 3 , y <= 4 },
+            { x >= 110 , x <= 123, y >= 4 , y <= 5 },
+            { x >= 110 , x <= 124, y >= 5 , y <= 6 },
+            { x >= 110 , x <= 125, y >= 6 , y <= 7 },
+            { x >= 110 , x <= 126, y >= 7 , y <= 8 },
+        })
+    };
+
+    Observable observable { Observable { AP({"p"}), observableInterpretation } };
+
+
+    std::unique_ptr<TileExtractor> tilextractor {};
+    SECTION("TileExtractorList")
+    {
+        tilextractor = std::make_unique<TileExtractorList>();
+        std::vector<Tile> tiles {};
+
+        BENCHMARK("TileExtractorList benchmark")
+        {
+            tiles = tilextractor->extractTiles(observable);
+        };
+
+        REQUIRE(tiles.size() == 8);
+    }
+
+    SECTION("TileExtractorComplex")
+    {
+        tilextractor = std::make_unique<TileExtractorComplex>();
+        std::vector<Tile> tiles {};
+
+        BENCHMARK("TileExtractorComplex benchmark")
+        {
+            tiles = tilextractor->extractTiles(observable);
+        };
+
+        REQUIRE(tiles.size() == 8);
+    }
+
+    SECTION("TileExtractorFast")
+    {
+        tilextractor = std::make_unique<TileExtractorFast>();
+        std::vector<Tile> tiles {};
+
+        BENCHMARK("TileExtractorFast benchmark")
+        {
+            tiles = tilextractor->extractTiles(observable);
+        };
+
+        REQUIRE(tiles.size() == 8);
+    }
+
+    SECTION("TileExtractorGraph")
+    {
+        tilextractor = std::make_unique<TileExtractorGraph>();
+        std::vector<Tile> tiles {};
+
+        BENCHMARK("TileExtractorGraph benchmark")
+        {
+            tiles = tilextractor->extractTiles(observable);
+        };
+
+        REQUIRE(tiles.size() == 8);
     }
 }
--- tests/tile/tilextractor_tests.cpp
+++ tests/tile/tilextractor_tests.cpp
@@ -674,4 +674,380 @@ TEST_CASE("Extract Tiles from Observables")
             );
         }
     }
+
+
+    SECTION(
+        "(\n"
+        "    { x >= -1, x <= 1, y >= -1, y <= 1 },\n"
+        "    { x >= 1 , x <= 2, y >= 0 , y <= 1 },\n"
+
+        "    { x >= 4 , x <= 5, y >= 0 , y <= 1 },\n"
+        "    { x >= 4 , x <= 6, y >= 1 , y <= 2 },\n"
+        "    { x >= 4 , x <= 7, y >= 2 , y <= 3 },\n"
+        "    { x >= 4 , x <= 8, y >= 3 , y <= 4 },\n"
+        "    { x >= 4 , x <= 9, y >= 4 , y <= 5 },\n"
+        "    { x >= 4 , x <= 10, y >= 5 , y <= 6 },\n"
+        "    { x >= 4 , x <= 11, y >= 7 , y <= 8 },\n"
+        "    { x >= 4 , x <= 12, y >= 8 , y <= 9 },\n"
+        "    { x >= 4 , x <= 13, y >= 9 , y <= 10 },\n"
+
+        "    { x >= 10 , x <= 11, y >= 0 , y <= 1 },\n"
+        "    { x >= 10 , x <= 12, y >= 1 , y <= 2 },\n"
+        "    { x >= 10 , x <= 13, y >= 2 , y <= 3 },\n"
+        "    { x >= 10 , x <= 14, y >= 3 , y <= 4 },\n"
+        "    { x >= 10 , x <= 15, y >= 4 , y <= 5 },\n"
+        "    { x >= 10 , x <= 16, y >= 5 , y <= 6 },\n"
+
+        "    { x >= 0 , x <= 1, y >= 7 , y <= 8 },\n"
+        "    { x >= 0 , x <= 1, y >= 8 , y <= 9 },\n"
+        "    { x >= 0 , x <= 2, y >= 8 , y <= 9 },\n"
+        "    { x >= 0 , x <= 3, y >= 9 , y <= 10 },\n"
+        "    { x >= 2 , x <= 3, y >= 9 , y <= 10 },\n"
+        "    { x >= 3 , x <= 4, y >= 9 , y <= 10 },\n"
+
+        "    { x >= -1 , x <= 0, y >= 6 , y <= 7 },\n"
+        "    { x >= -2 , x <= -1, y >= 5 , y <= 6 },\n"
+        "    { x >= -2 , x <= -1, y >= 6 , y <= 7 },\n"
+        "    { x >= 15 , x <= 16, y >= 3 , y <= 4 },\n"
+        "    { x >= 15 , x <= 17, y >= 4 , y <= 5 },\n"
+
+        "    { x >= 17 , x <= 18, y >= 5 , y <= 6 },\n"
+        "    { x >= 17 , x <= 18, y >= 4 , y <= 5 },\n"
+        "    { x >= 17 , x <= 18, y >= 3 , y <= 4 },\n"
+        "    { x >= -3 , x <= -2, y >= 3 , y <= 4 },\n"
+        "    { x >= -3 , x <= -2, y >= 4 , y <= 5 },\n"
+
+        "    { x >= 50 , x <= 52, y >= 50 , y <= 50 },\n"
+        "    { x >= 50 , x <= 53, y >= 50 , y <= 52 },\n"
+        "    { x >= 50 , x <= 54, y >= 52 , y <= 53 },\n"
+        "    { x >= 50 , x <= 55, y >= 53 , y <= 54 },\n"
+        "    { x >= 50 , x <= 56, y >= 54 , y <= 55 },\n"
+        "    { x >= 50 , x <= 57, y >= 55 , y <= 56 },\n"
+        "    { x >= 50 , x <= 58, y >= 56 , y <= 57 },\n"
+        "    { x >= 50 , x <= 59, y >= 57 , y <= 58 },\n"
+
+        "    { x >= 70 , x <= 72, y >= 70 , y <= 70 },\n"
+        "    { x >= 70 , x <= 73, y >= 70 , y <= 72 },\n"
+        "    { x >= 70 , x <= 74, y >= 72 , y <= 73 },\n"
+        "    { x >= 70 , x <= 75, y >= 73 , y <= 74 },\n"
+        "    { x >= 70 , x <= 76, y >= 74 , y <= 75 },\n"
+        "    { x >= 70 , x <= 77, y >= 75 , y <= 76 },\n"
+        "    { x >= 70 , x <= 78, y >= 76 , y <= 77 },\n"
+        "    { x >= 70 , x <= 79, y >= 77 , y <= 78 },\n"
+
+        "    { x >= 90 , x <= 92, y >= 90 , y <= 90 },\n"
+        "    { x >= 90 , x <= 93, y >= 90 , y <= 92 },\n"
+        "    { x >= 90 , x <= 94, y >= 92 , y <= 93 },\n"
+        "    { x >= 90 , x <= 95, y >= 93 , y <= 94 },\n"
+        "    { x >= 90 , x <= 96, y >= 94 , y <= 95 },\n"
+        "    { x >= 90 , x <= 97, y >= 95 , y <= 96 },\n"
+        "    { x >= 90 , x <= 98, y >= 96 , y <= 97 },\n"
+        "    { x >= 90 , x <= 99, y >= 97 , y <= 98 },\n"
+
+        "    { x >= 110 , x <= 112, y >= 110 , y <= 110 },\n"
+        "    { x >= 110 , x <= 113, y >= 110 , y <= 112 },\n"
+        "    { x >= 110 , x <= 114, y >= 112 , y <= 113 },\n"
+        "    { x >= 110 , x <= 115, y >= 113 , y <= 114 },\n"
+        "    { x >= 110 , x <= 116, y >= 114 , y <= 115 },\n"
+        "    { x >= 110 , x <= 117, y >= 115 , y <= 116 },\n"
+        "    { x >= 110 , x <= 118, y >= 116 , y <= 117 },\n"
+        "    { x >= 110 , x <= 119, y >= 117 , y <= 118 },\n"
+
+        "    { x >= 110 , x <= 120, y >= 1 , y <= 2 },\n"
+        "    { x >= 110 , x <= 121, y >= 2 , y <= 3 },\n"
+        "    { x >= 110 , x <= 122, y >= 3 , y <= 4 },\n"
+        "    { x >= 110 , x <= 123, y >= 4 , y <= 5 },\n"
+        "    { x >= 110 , x <= 124, y >= 5 , y <= 6 },\n"
+        "    { x >= 110 , x <= 125, y >= 6 , y <= 7 },\n"
+        "    { x >= 110 , x <= 126, y >= 7 , y <= 8 }\n"
+        ")"
+    )
+    {
+        PPL::Variable x { 0 };
+        PPL::Variable y { 1 };
+
+        Powerset observableInterpretation {
+            PPLUtils::powerset({
+                { x >= -1, x <= 1, y >= -1, y <= 1 },
+                { x >= 1 , x <= 2, y >= 0 , y <= 1 },
+
+                { x >= 4 , x <= 5, y >= 0 , y <= 1 },
+                { x >= 4 , x <= 6, y >= 1 , y <= 2 },
+                { x >= 4 , x <= 7, y >= 2 , y <= 3 },
+                { x >= 4 , x <= 8, y >= 3 , y <= 4 },
+                { x >= 4 , x <= 9, y >= 4 , y <= 5 },
+                { x >= 4 , x <= 10, y >= 5 , y <= 6 },
+                { x >= 4 , x <= 11, y >= 7 , y <= 8 },
+                { x >= 4 , x <= 12, y >= 8 , y <= 9 },
+                { x >= 4 , x <= 13, y >= 9 , y <= 10 },
+
+                { x >= 10 , x <= 11, y >= 0 , y <= 1 },
+                { x >= 10 , x <= 12, y >= 1 , y <= 2 },
+                { x >= 10 , x <= 13, y >= 2 , y <= 3 },
+                { x >= 10 , x <= 14, y >= 3 , y <= 4 },
+                { x >= 10 , x <= 15, y >= 4 , y <= 5 },
+                { x >= 10 , x <= 16, y >= 5 , y <= 6 },
+
+                { x >= 0 , x <= 1, y >= 7 , y <= 8 },
+                { x >= 0 , x <= 1, y >= 8 , y <= 9 },
+                { x >= 0 , x <= 2, y >= 8 , y <= 9 },
+                { x >= 0 , x <= 3, y >= 9 , y <= 10 },
+                { x >= 2 , x <= 3, y >= 9 , y <= 10 },
+                { x >= 3 , x <= 4, y >= 9 , y <= 10 },
+
+                { x >= -1 , x <= 0, y >= 6 , y <= 7 },
+                { x >= -2 , x <= -1, y >= 5 , y <= 6 },
+                { x >= -2 , x <= -1, y >= 6 , y <= 7 },
+                { x >= 15 , x <= 16, y >= 3 , y <= 4 },
+                { x >= 15 , x <= 17, y >= 4 , y <= 5 },
+
+                { x >= 17 , x <= 18, y >= 5 , y <= 6 },
+                { x >= 17 , x <= 18, y >= 4 , y <= 5 },
+                { x >= 17 , x <= 18, y >= 3 , y <= 4 },
+                { x >= -3 , x <= -2, y >= 3 , y <= 4 },
+                { x >= -3 , x <= -2, y >= 4 , y <= 5 },
+
+                { x >= 50 , x <= 52, y >= 50 , y <= 50 },
+                { x >= 50 , x <= 53, y >= 50 , y <= 52 },
+                { x >= 50 , x <= 54, y >= 52 , y <= 53 },
+                { x >= 50 , x <= 55, y >= 53 , y <= 54 },
+                { x >= 50 , x <= 56, y >= 54 , y <= 55 },
+                { x >= 50 , x <= 57, y >= 55 , y <= 56 },
+                { x >= 50 , x <= 58, y >= 56 , y <= 57 },
+                { x >= 50 , x <= 59, y >= 57 , y <= 58 },
+
+                { x >= 70 , x <= 72, y >= 70 , y <= 70 },
+                { x >= 70 , x <= 73, y >= 70 , y <= 72 },
+                { x >= 70 , x <= 74, y >= 72 , y <= 73 },
+                { x >= 70 , x <= 75, y >= 73 , y <= 74 },
+                { x >= 70 , x <= 76, y >= 74 , y <= 75 },
+                { x >= 70 , x <= 77, y >= 75 , y <= 76 },
+                { x >= 70 , x <= 78, y >= 76 , y <= 77 },
+                { x >= 70 , x <= 79, y >= 77 , y <= 78 },
+
+                { x >= 90 , x <= 92, y >= 90 , y <= 90 },
+                { x >= 90 , x <= 93, y >= 90 , y <= 92 },
+                { x >= 90 , x <= 94, y >= 92 , y <= 93 },
+                { x >= 90 , x <= 95, y >= 93 , y <= 94 },
+                { x >= 90 , x <= 96, y >= 94 , y <= 95 },
+                { x >= 90 , x <= 97, y >= 95 , y <= 96 },
+                { x >= 90 , x <= 98, y >= 96 , y <= 97 },
+                { x >= 90 , x <= 99, y >= 97 , y <= 98 },
+
+                { x >= 110 , x <= 112, y >= 110 , y <= 110 },
+                { x >= 110 , x <= 113, y >= 110 , y <= 112 },
+                { x >= 110 , x <= 114, y >= 112 , y <= 113 },
+                { x >= 110 , x <= 115, y >= 113 , y <= 114 },
+                { x >= 110 , x <= 116, y >= 114 , y <= 115 },
+                { x >= 110 , x <= 117, y >= 115 , y <= 116 },
+                { x >= 110 , x <= 118, y >= 116 , y <= 117 },
+                { x >= 110 , x <= 119, y >= 117 , y <= 118 },
+
+                { x >= 110 , x <= 120, y >= 1 , y <= 2 },
+                { x >= 110 , x <= 121, y >= 2 , y <= 3 },
+                { x >= 110 , x <= 122, y >= 3 , y <= 4 },
+                { x >= 110 , x <= 123, y >= 4 , y <= 5 },
+                { x >= 110 , x <= 124, y >= 5 , y <= 6 },
+                { x >= 110 , x <= 125, y >= 6 , y <= 7 },
+                { x >= 110 , x <= 126, y >= 7 , y <= 8 },
+            })
+        };
+        REQUIRE(observableInterpretation.size() == 72);
+
+        Observable observable { Observable { AP({"p"}), observableInterpretation } };
+        Tile expectedTile1 {
+            observable,
+            PPLUtils::powerset({
+                { -x >= -2, -y >= -1, y >= 0, x >= 1 },
+                { -x >= -1, -y >= -1, y >= -1, x >= -1 }
+            })
+        };
+        Tile expectedTile2 {
+            observable,
+            PPLUtils::powerset({
+                { -x >= -6, -y >= -2, y >= 1, x >= 4 },
+                { -x >= -5, -y >= -1, y >= 0, x >= 4 },
+                { -x >= -7, -y >= -3, y >= 2, x >= 4 },
+                { -x >= -8, -y >= -4, y >= 3, x >= 4 },
+                { -x >= -9, -y >= -5, y >= 4, x >= 4 },
+                { -x >= -10, -y >= -6, y >= 5, x >= 4 },
+                { -x >= -15, -y >= -5, y >= 4, x >= 10 },
+                { -x >= -14, -y >= -4, y >= 3, x >= 10 },
+                { -x >= -13, -y >= -3, y >= 2, x >= 10 },
+                { -x >= -12, -y >= -2, y >= 1, x >= 10 },
+                { -x >= -11, -y >= -1, y >= 0, x >= 10 },
+                { -x >= -16, -y >= -6, y >= 5, x >= 10 },
+                { -x >= -17, -y >= -5, y >= 4, x >= 15 },
+                { -x >= -16, -y >= -4, y >= 3, x >= 15 },
+                { -x >= -18, -y >= -5, y >= 4, x >= 17 },
+                { -x >= -18, -y >= -6, y >= 5, x >= 17 },
+                { -x >= -18, -y >= -4, y >= 3, x >= 17 }
+            })
+        };
+        Tile expectedTile3 {
+            observable,
+            PPLUtils::powerset({
+                { -x >= -12, -y >= -9, y >= 8, x >= 4 },
+                { -x >= -11, -y >= -8, y >= 7, x >= 4 },
+                { -x >= -13, -y >= -10, y >= 9, x >= 4 },
+                { -x >= -4, -y >= -10, y >= 9, x >= 3 },
+                { -x >= -2, -y >= -9, y >= 8, x >= 0 },
+                { -x >= -1, -y >= -8, y >= 7, x >= 0 },
+                { -x >= -3, -y >= -10, y >= 9, x >= 0 },
+                { -x >= 0, -y >= -7, y >= 6, x >= -1 },
+                { -x >= 1, -y >= -7, y >= 6, x >= -2 },
+                { -x >= 1, -y >= -6, y >= 5, x >= -2 },
+                { -x >= 2, -y >= -5, y >= 4, x >= -3 },
+                { -x >= 2, -y >= -4, y >= 3, x >= -3 }
+            })
+        };
+        Tile expectedTile4 {
+            observable,
+            PPLUtils::powerset({
+                { -x >= -53, -y >= -52, y >= 50, x >= 50 },
+                { -x >= -54, -y >= -53, y >= 52, x >= 50 },
+                { -x >= -55, -y >= -54, y >= 53, x >= 50 },
+                { -x >= -56, -y >= -55, y >= 54, x >= 50 },
+                { -x >= -57, -y >= -56, y >= 55, x >= 50 },
+                { -x >= -58, -y >= -57, y >= 56, x >= 50 },
+                { -x >= -59, -y >= -58, y >= 57, x >= 50 }
+            })
+        };
+        Tile expectedTile5 {
+            observable,
+            PPLUtils::powerset({
+                { -x >= -73, -y >= -72, y >= 70, x >= 70 },
+                { -x >= -74, -y >= -73, y >= 72, x >= 70 },
+                { -x >= -75, -y >= -74, y >= 73, x >= 70 },
+                { -x >= -76, -y >= -75, y >= 74, x >= 70 },
+                { -x >= -77, -y >= -76, y >= 75, x >= 70 },
+                { -x >= -78, -y >= -77, y >= 76, x >= 70 },
+                { -x >= -79, -y >= -78, y >= 77, x >= 70 }
+            })
+        };
+        Tile expectedTile6 {
+            observable,
+            PPLUtils::powerset({
+                { -x >= -93, -y >= -92, y >= 90, x >= 90 },
+                { -x >= -94, -y >= -93, y >= 92, x >= 90 },
+                { -x >= -95, -y >= -94, y >= 93, x >= 90 },
+                { -x >= -96, -y >= -95, y >= 94, x >= 90 },
+                { -x >= -97, -y >= -96, y >= 95, x >= 90 },
+                { -x >= -98, -y >= -97, y >= 96, x >= 90 },
+                { -x >= -99, -y >= -98, y >= 97, x >= 90 }
+            })
+        };
+        Tile expectedTile7 {
+            observable,
+            PPLUtils::powerset({
+                { -x >= -113, -y >= -112, y >= 110, x >= 110 },
+                { -x >= -114, -y >= -113, y >= 112, x >= 110 },
+                { -x >= -115, -y >= -114, y >= 113, x >= 110 },
+                { -x >= -116, -y >= -115, y >= 114, x >= 110 },
+                { -x >= -117, -y >= -116, y >= 115, x >= 110 },
+                { -x >= -118, -y >= -117, y >= 116, x >= 110 },
+                { -x >= -119, -y >= -118, y >= 117, x >= 110 }
+            })
+        };
+        Tile expectedTile8 {
+            observable,
+            PPLUtils::powerset({
+                { -x >= -120, -y >= -2, y >= 1, x >= 110 },
+                { -x >= -121, -y >= -3, y >= 2, x >= 110 },
+                { -x >= -122, -y >= -4, y >= 3, x >= 110 },
+                { -x >= -123, -y >= -5, y >= 4, x >= 110 },
+                { -x >= -124, -y >= -6, y >= 5, x >= 110 },
+                { -x >= -125, -y >= -7, y >= 6, x >= 110 },
+                { -x >= -126, -y >= -8, y >= 7, x >= 110 }
+            })
+        };
+
+        SECTION("TileExtractorComplex")
+        {
+            TileExtractorComplex tileExtractor {};
+            std::vector tiles { tileExtractor.extractTiles(observable) };
+
+
+            REQUIRE(tiles.size() == 9);
+            REQUIRE_THAT(
+                tiles,
+                Catch::Matchers::UnorderedEquals(std::vector {
+                    expectedTile1,
+                    expectedTile2,
+                    expectedTile3,
+                    expectedTile4,
+                    expectedTile5,
+                    expectedTile6,
+                    expectedTile7,
+                    expectedTile8,
+                })
+            );
+        }
+
+        SECTION("TileExtractorList")
+        {
+            TileExtractorList tileExtractorList {};
+            std::vector tiles { tileExtractorList.extractTiles(observable) };
+
+
+            REQUIRE(tiles.size() == 8);
+            REQUIRE_THAT(
+                tiles,
+                Catch::Matchers::UnorderedEquals(std::vector {
+                    expectedTile1,
+                    expectedTile2,
+                    expectedTile3,
+                    expectedTile4,
+                    expectedTile5,
+                    expectedTile6,
+                    expectedTile7,
+                    expectedTile8,
+                })
+            );
+        }
+
+        SECTION("TileExtractorFast")
+        {
+            TileExtractorFast tileExtractorFast {};
+            std::vector tiles { tileExtractorFast.extractTiles(observable) };
+
+
+            REQUIRE(tiles.size() == 8);
+            REQUIRE_THAT(
+                tiles,
+                Catch::Matchers::UnorderedEquals(std::vector {
+                    expectedTile1,
+                    expectedTile2,
+                    expectedTile3,
+                    expectedTile4,
+                    expectedTile5,
+                    expectedTile6,
+                    expectedTile7,
+                    expectedTile8,
+                })
+            );
+        }
+
+        SECTION("TileExtractorGraph")
+        {
+            TileExtractorGraph tileExtractorGraph {};
+            std::vector tiles { tileExtractorGraph.extractTiles(observable) };
+
+
+            REQUIRE(tiles.size() == 8);
+            REQUIRE_THAT(
+                tiles,
+                Catch::Matchers::UnorderedEquals(std::vector {
+                    expectedTile1,
+                    expectedTile2,
+                    expectedTile3,
+                    expectedTile4,
+                    expectedTile5,
+                    expectedTile6,
+                    expectedTile7,
+                    expectedTile8,
+                })
+            );
+        }
+    }
 }
